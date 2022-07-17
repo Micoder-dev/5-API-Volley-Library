bea39f058f9bf265b36cb3912bab89a8ea67984b
Micoder-dev <micoder.com@gmail.com>
2022-07-18 00:47:46 +0530

    accessing, retreiving the JSON data & displayed in the LOG

--- app/src/main/java/com/example/volleytestapp/MainActivity.java
+++ app/src/main/java/com/example/volleytestapp/MainActivity.java
@@ -3,12 +3,49 @@ package com.example.volleytestapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    RequestQueue mRequestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRequestQueue = Volley.newRequestQueue(this);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                "https://joke.deno.dev/",
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        Log.i("RESPONSEVALIDDATA", response + "");
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        Log.e("RESPONSEERROR", error.getMessage());
+
+                    }
+                });
+
+        mRequestQueue.add(jsonObjectRequest);
+
     }
 }
