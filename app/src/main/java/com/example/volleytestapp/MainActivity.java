dd27b0599a73574bb5c5c5c8b75c1ab6f92c278c
Micoder-dev <micoder.com@gmail.com>
2022-07-18 08:39:52 +0530

    accessing, retreiving the JSON ARRAY data & displayed in the LOG

--- app/src/main/java/com/example/volleytestapp/MainActivity.java
+++ app/src/main/java/com/example/volleytestapp/MainActivity.java
@@ -9,14 +9,17 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     RequestQueue mRequestQueue;
+    RequestQueue mRequestQueueArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mRequestQueue = Volley.newRequestQueue(this);
+        mRequestQueueArray = Volley.newRequestQueue(this);
 
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
                 "https://joke.deno.dev/",
@@ -46,6 +50,27 @@ public class MainActivity extends AppCompatActivity {
                 });
 
         mRequestQueue.add(jsonObjectRequest);
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET,
+                "https://joke.deno.dev/type/general/10",
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+
+                        Log.i("ARRAYRESPONSE", response + "");
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        Log.e("ARRAYERROR", error.getMessage());
+
+                    }
+                });
+        mRequestQueueArray.add(jsonArrayRequest);
 
     }
 }
